10e9acc3752044b4f6b726de445af770bad94d9c
heitorrapela <hrm@cin.ufpe.br>
2020-10-23 20:33:51 -0300

    update cartpole

--- getting_start_with_gym/cartPole-v0.py
+++ getting_start_with_gym/cartPole-v0.py
@@ -1,9 +1,35 @@
 import gym
 
-env = gym.make('CartPole-v0')
-env.reset()
+debug = False
+env_number = 0
+choose_env = ["CartPole-v0", "MountainCar-v0", "MsPacman-v0", "Hopper-v1"]
 
-for _ in range(1000):
-	env.render()
-	env.step(env.action_space.sample())
+env = gym.make(choose_env[env_number])
+
+print(env.action_space)
+
+print(env.observation_space)
+
+print(env.observation_space.high)
+print(env.observation_space.low)
+
+for i_episode in range(20):
+	observation = env.reset()
+
+	for t in range(100):
+		env.render()
+		observation, reward, done, info = env.step(env.action_space.sample())
+		
+		if debug:
+			print("Observation: ", observation)
+			print("Reward: ", reward)
+			print("Done: ", done)
+			print("Info: ", info)
+
+		if done:
+			print("Episode finished after {} timesteps".format(t+1))
+			break
+
 env.close()
+
+
